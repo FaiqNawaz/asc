4c10456f82684d7589495a04ad7e60f0df953801
FaiqNawaz <faiqnawaz92@gmail.com>
2025-04-23 22:38:19 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Hero from './Components/Hero'
+
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
